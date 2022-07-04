7ff9cb44dc79a3f47836cfd16f7c40fec5f19b10
ShishirShekhar <sspdav02@gmail.com>
2022-07-04 18:38:33 +0530

    added variation in navbar

--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Nav from '../components/Nav';
 
 const Rooms = () => {
   return (
-    <div>Rooms</div>
+    <div>
+      <Nav minimal={false} />
+    </div>
   );
 };
 
