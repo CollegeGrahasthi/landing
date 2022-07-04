36fb7da32e934b9c84bcc750eae0ae74d46a6d3e
ShishirShekhar <sspdav02@gmail.com>
2022-07-04 18:07:40 +0530

    fixing multipage error v3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Rooms from './pages/Rooms';
 import Home from './pages/Home';
 
 const App = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path={'/'} element={<Home />} />
         <Route path={'/rooms'} element={<Rooms />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
